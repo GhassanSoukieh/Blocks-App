79412bf80677fc8dd05ab9c7efbff3ade651c589
Ghassan Soukieh <ghassan.soukieh@gracestudio.se>
2025-05-21 22:20:51 +0200

    Security update.

--- firebase.js
+++ firebase.js
@@ -2,19 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTiauwq0LkW-Grn8sI9IviGrVu9sxMqx4",
-  authDomain: "blocks-e078e.firebaseapp.com",
-  projectId: "blocks-e078e",
-  storageBucket: "blocks-e078e.firebasestorage.app",
-  messagingSenderId: "430286614651",
-  appId: "1:430286614651:web:38aa5830607d50f32632de",
-  measurementId: "G-15L701C9VT",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
